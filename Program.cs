bc0bb96e850dc61ab642187d24e95f3def99dec5
fd <fd@MSHL80489>
2024-06-20 14:15:51 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rename
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben Sie Bitte ");
+            Console.WriteLine("Hallo");
+        }
+    }
+}
