36eb3a237fefd72bfeb8261b02322659fce7bca1
Filippowa, Ksenia <kfilippowa@motorpresse.de>
2024-06-27 11:56:45 +0200

    Test

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace rename
 {
@@ -10,7 +6,7 @@ namespace rename
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Geben Sie Bitte ");
+            Console.WriteLine("");
             Console.WriteLine("Hallo");
         }
     }
