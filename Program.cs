846faa9720360a6dc93182b03848612ac4a17db2
Fatima Dawara <Fatima.Dawara@msh.de>
2024-07-02 08:25:21 +0200

    get file names

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,22 @@ namespace rename
 {
     internal class Program
     {
+
         static void Main(string[] args)
-        {
-            Console.WriteLine("Geben Sie Bitte ");
-            Console.WriteLine("Hallo");
+        { // Datei zugreifen
+            var folderPath = @"C:\Users\fd\Desktop\Projekt_Rename\Biler\";
+            var fileEntries = Directory.GetFiles(folderPath);
+            string[] fileNamen = new string[fileEntries.Length];
+
+            foreach (var fileEntry in fileEntries)
+                {
+                    var filename = Path.GetFileName(fileEntry);
+                    Console.WriteLine(filename);
+                }
+            
+            Console.ReadKey();
+           
+
         }
     }
 }
