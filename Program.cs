c2dea587108649e180a56d6c9918b95ad1bdc970
Filippowa, Ksenia <kfilippowa@motorpresse.de>
2024-07-02 08:38:30 +0200

    Nachtrag

--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace rename
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("");
-            Console.WriteLine("Hallo");
-        }
+		{
+			
+		}
+
+		static string[] ReadFileNames()
+		{
+			string folderPath = @"C:\Users\fd\Desktop\Projekt_Rename\Biler\";
+			string[] fileEntries = Directory.GetFiles(folderPath);
+			string[] fileNamen = new string[fileEntries.Length];
+
+			foreach (string fileEntry in fileEntries)
+			{
+				string filename = Path.GetFileName(fileEntry);
+				fileNamen.Append(filename);
+			}
+
+			return fileNamen;
+
+		}
     }
 }
