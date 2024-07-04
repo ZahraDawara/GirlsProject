8863231f78908c97add274180357f87c9af0961f
Fatima Dawara <Fatima.Dawara@msh.de>
2024-07-04 14:46:50 +0200

    Praefix und Suffix aendern und entfernen

--- Program.cs
+++ Program.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Web;
+using System.Xml.Linq;
 
 namespace rename
 {
-    internal class Program
-    {
+	internal class Program
+	{
 
-        static void Main(string[] args)
+		static void Main(string[] args)
 		{
+			string[] fileNamen = ReadFileNames();
+			 changeRaeflixName(fileNamen);
 			
 		}
 
@@ -19,14 +23,65 @@ namespace rename
 			string[] fileEntries = Directory.GetFiles(folderPath);
 			string[] fileNamen = new string[fileEntries.Length];
 
-			foreach (string fileEntry in fileEntries)
+			for (int i = 0; i < fileEntries.Length; i++)
 			{
-				string filename = Path.GetFileName(fileEntry);
-				fileNamen.Append(filename);
+
+				string filename = Path.GetFileName(fileEntries[i]);
+				fileNamen[i] = filename;
 			}
 
 			return fileNamen;
 
 		}
- }
+		static Dictionary<string, string> changeRaeflixName(string[] fileNamen)
+		{
+			string[] changeRaeflixName = new string[fileNamen.Length];
+
+			//Array.Copy(fileNamen, fileNameChange, fileNamen.Length);
+			Dictionary<string, string> mydictionary = new Dictionary<string, string>();
+
+			for (int i = 0; i < fileNamen.Length; i++)
+			{
+				if (fileNamen[i].StartsWith("img-"))
+				{
+
+					changeRaeflixName[i] = "Image-" + fileNamen[i].Substring(4);
+					mydictionary.Add(fileNamen[i], changeRaeflixName[i]);
+				}
+			}
+			return mydictionary;
+		}
+		static Dictionary<string,string> changeSuffixnamen(string[] fileNamen, string suffix)
+		{
+            string[] changeSuffixName = new string[fileNamen.Length];
+            Dictionary<string, string> mydictionary = new Dictionary<string, string>();
+			
+			for(int i = 0;i < fileNamen.Length; i++)
+			{
+				if (fileNamen[i].EndsWith("jpg"))
+				{
+					int fileNameLen = fileNamen[i].Count();
+					changeSuffixName[i] = fileNamen[i].Substring(0, fileNameLen-3)+suffix;
+					mydictionary.Add(fileNamen[i], changeSuffixName[i]);
+				}
+			}
+            return mydictionary;
+        }
+		static Dictionary<string,string> deletePraefixName(string[] fileNamen)
+		{
+            string[] deletePraefixName = new string[fileNamen.Length];
+            Dictionary<string, string> mydictionary = new Dictionary<string, string>();
+            for (int i = 0; i < fileNamen.Length; i++)
+			{
+				if (fileNamen[i].StartsWith("img-"))
+				{
+					string newFilename = fileNamen[i].Substring(4);
+					mydictionary.Add(fileNamen[i], deletePraefixName[i]);
+				}
+			}
+			return mydictionary;
+
+		}
+
+	}
 }
